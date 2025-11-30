9c5950309743f1f261f06049f3e6781d7d290ae7
MaakCode <11623298+maakcode@users.noreply.github.com>
2025-11-30 15:18:09 +0900

    Fix bug where logo is not visible in release mode
    https://github.com/orgs/tauri-apps/discussions/6423#discussioncomment-5277253

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,9 @@ export default defineConfig(async () => ({
   //
   // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
+  build: {
+    assetsInlineLimit: 0,
+  },
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
